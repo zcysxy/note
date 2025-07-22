3229c46f51c717797adaf7475c511bc910e4b994
zcysxy <zcysxy.wyzdz@gmail.com>
2025-07-22 15:37:58 -0400

    merge

--- quartz/plugins/transformers/tikz.ts
+++ quartz/plugins/transformers/tikz.ts
@@ -4,31 +4,52 @@ import { QuartzTransformerPlugin } from "../types"
 // Change in Markdown syntax tree from codeblock to script tag
 export const TikzTransformer: QuartzTransformerPlugin = () => {
   return {
-    name: 'TikzTransformer',
+    name: "TikzTransformer",
+    externalResources() {
+      return {
+        js: [
+          {
+            src: "https://tikzjax.com/v1/tikzjax.js",
+            loadTime: "beforeDOMReady",
+            contentType: "external",
+            async: true,
+          },
+        ],
+        css: [
+          {
+            src: "http://tikzjax.com/v1/fonts.css",
+            loadTime: "beforeDOMReady",
+            contentType: "external",
+          },
+        ],
+      }
+    },
     markdownPlugins() {
-      return [() => {
-        return (tree, file) => {
-          visit(tree, 'code', (node) => {
-            if (node.lang === 'tikz') {
-              // Clean up Latex code
-              const remove = "&nbsp;";
-              node.value = node.value.replaceAll(remove, "");
+      return [
+        () => {
+          return (tree, file) => {
+            visit(tree, "code", (node) => {
+              if (node.lang === "tikz") {
+                // Clean up Latex code
+                const remove = "&nbsp;"
+                node.value = node.value.replaceAll(remove, "")
 
-              let lines = node.value.split("\n");
+                let lines = node.value.split("\n")
 
-              // Trim whitespace that is inserted when pasting in code, otherwise TikZJax complains
-              lines = lines.map(line => line.trim());
+                // Trim whitespace that is inserted when pasting in code, otherwise TikZJax complains
+                lines = lines.map((line) => line.trim())
 
-              // Remove empty lines
-              lines = lines.filter(line => line);
+                // Remove empty lines
+                lines = lines.filter((line) => line)
 
-              node.value = lines.join("\n");
-              node.type = 'html';
-              node.value = `<script type="text/tikz"'>${node.value}</script>`;
-            }
-          });
-        };
-      }];
+                node.value = lines.join("\n")
+                node.type = "html"
+                node.value = `<script type="text/tikz"'>${node.value}</script>`
+              }
+            })
+          }
+        },
+      ]
     },
     htmlPlugins() {
       return [
@@ -37,38 +58,27 @@ export const TikzTransformer: QuartzTransformerPlugin = () => {
             // Change node to include raw HTML before parsing to JSX
             visit(tree, "element", (node, index, parent) => {
               if (node.tagName === "script" && node.properties?.type === "text/tikz") {
-                  const divNode = {
-                    type: 'element',
-                    tagName: 'div',
-                    properties: { dangerouslySetInnerHTML: { __html: '<script type="text/tikz">'.concat(node.children[0].value, '</script>')} },
-                    children: []
-                  };
-                  parent.children[index] = divNode
+                const divNode = {
+                  type: "element",
+                  tagName: "div",
+                  properties: {
+                    dangerouslySetInnerHTML: {
+                      __html: '<script type="text/tikz">'.concat(
+                        node.children[0].value,
+                        "</script>",
+                      ),
+                    },
+                  },
+                  children: [],
+                }
+                parent.children[index] = divNode
               }
             })
 
             // Add the TikZJax stylesheet and script tag to the head
-            tree.children.unshift({
-              type: 'element',
-              tagName: 'script',
-              properties: {
-                src: '../../static/tikzjax.js',
-              },
-              children: [],
-            });
-            tree.children.unshift({
-              type: 'element',
-              tagName: 'link',
-              properties: {
-                rel: 'stylesheet',
-                type: 'text/css',
-                href: '../../static/fonts.css',
-              },
-              children: [],
-            });
-          };
+          }
         },
-      ];
+      ]
     },
-  };
-}; 
+  }
+}
